e118b66df86d3f9d23fbf7f3a7cec51318b8aff6
Vinicius Santos <viniciusantos.florencio@gmail.com>
2022-10-18 19:48:34 -0300

    Cria tela inicial

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,51 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { COLORS } from './globals/style';
+import { KeyboardAvoidingView, ScrollView, StyleSheet, View } from 'react-native';
 
-export default function App() {
+import Logo from './assets/logo.svg';
+import Input from './components/Input';
+import Button from './components/Button';
+import Title from './components/Title';
+
+
+export default function App() {    
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior='padding'>
+           <ScrollView showsVerticalScrollIndicator={false}>
+           <StatusBar style="auto" />
+            <View style={{alignItems: 'center', marginTop: 100 }}>
+                <Logo width={200} height={200}/>
+            </View>
+
+            <View style={styles.contentContainer}>
+                <Title>Login</Title>
+                
+                <View style={styles.formContainer}>
+                    <Input placeholder='Email ID' iconName="email-outline"/>
+                    <View style={{ marginVertical: 10 }}/>
+                    <Input placeholder='Password'iconName="lock-outline"/>
+                </View>
+                
+                <Button title="Login" />
+            </View>
+           </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: COLORS.white,
   },
+
+  formContainer: {
+    marginVertical: 40, 
+    marginRight: 20
+  },
+
+  contentContainer: {
+    margin: 20,
+  }
 });
