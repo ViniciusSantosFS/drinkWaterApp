8ea601b70649714a2e358762a99e6f0d7ff3292d
Vinicius Santos <viniciusantos.florencio@gmail.com>
2022-10-18 20:56:59 -0300

    Cria estilo para login

---
+++ src/screens/Login/style.ts
@@ -0,0 +1,27 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+import { COLORS } from '~/globals/style';
+
+const { height } = Dimensions.get('window');
+const FIFTEEN_PERCENTE_SCREEN_HEIGHT = height * 0.15;
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+
+  logoContainer: {
+    alignItems: 'center',
+    marginTop: FIFTEEN_PERCENTE_SCREEN_HEIGHT,
+  },
+
+  formContainer: {
+    marginVertical: 40,
+    marginRight: 20,
+  },
+
+  contentContainer: {
+    margin: 20,
+  },
+});
