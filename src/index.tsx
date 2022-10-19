92729a66f147b662d8e0829dbd4ce4879ee61d92
Vinicius Santos <viniciusantos.florencio@gmail.com>
2022-10-18 21:20:27 -0300

    Adiciona navegação ao projeto

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import Home from '~/screens/Home';
-import Login from '~/screens/Login';
+import Router from './router';
 
 export default function App() {
-  return <Home />;
+  return <Router />;
 }
