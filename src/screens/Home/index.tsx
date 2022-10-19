cedd26bd92f17521629acaed64ec4b5bd2fde581
Vinicius Santos <viniciusantos.florencio@gmail.com>
2022-10-19 20:23:01 -0300

    Agenda tempo para notificações

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 
+import * as Notifications from '../../services/notification';
+import * as Storage from '../../services/storage';
 import Greetings from './components/Greetings';
 
 import IconButton from '~/components/IconButton';
 import Title from '~/components/Title';
 import { COLORS } from '~/globals/style';
 
+const DEFAULT_SCHEDULE_TIME = 60000;
+
 export default function Home() {
+  const configPushTimeSchedule = async () => {
+    await Promise.all([
+      Notifications.removeScheduleNotifications(),
+      Storage.updateScheduleTime(DEFAULT_SCHEDULE_TIME),
+    ]);
+
+    return Notifications.schedulePushNotification();
+  };
+
+  useEffect(() => {
+    configPushTimeSchedule().catch((error) => {
+      throw new Error(error);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <View>
