8d821843972667c561ccce1e31f51650d08b73b4
Vinicius Santos <viniciusantos.florencio@gmail.com>
2022-10-19 20:22:39 -0300

    Adiciona tela de permissões

---
+++ src/screens/RequestPermissions/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Greetings from '~/assets/greetings.svg';
+
+import AskPermissionsButton from './Containers/AskPermissionsButton';
+
+import Title from '~/components/Title';
+import { COLORS } from '~/globals/style';
+
+export default function RequestPermissions() {
+  return (
+    <View style={styles.container}>
+      <Title>
+        <Text style={{ textAlign: 'center' }}>Para te manter hidratado</Text>
+      </Title>
+
+      <Greetings width={200} height={200} />
+
+      <Text style={styles.description}>
+        Afim de te proporcionar uma melhor experiência é necessário aceitar
+        nossas permissões de uso
+      </Text>
+
+      <View style={{ width: '100%' }}>
+        <AskPermissionsButton />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: COLORS.white,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+
+  description: {
+    color: COLORS.gray,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
