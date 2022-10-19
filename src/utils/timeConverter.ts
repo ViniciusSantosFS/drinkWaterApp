74bb104018a281bb8d7b8e87b06cc522a6b771f9
Vinicius Santos <viniciusantos.florencio@gmail.com>
2022-10-19 20:21:48 -0300

    Funçõs utilitarias para converter tempo

---
+++ src/utils/timeConverter.ts
@@ -0,0 +1,5 @@
+const convertSecondsToMs = (seconds: number) => seconds * 1000;
+
+const convertMsToSeconds = (milliseconds: number) => milliseconds / 1000;
+
+export { convertMsToSeconds, convertSecondsToMs };
