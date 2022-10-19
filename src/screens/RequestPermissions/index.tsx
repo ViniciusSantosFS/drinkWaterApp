125f4a253448db17a1662eace6dbe2503b7148f1
Vinicius Santos <viniciusantos.florencio@gmail.com>
2022-10-19 20:29:50 -0300

    Ajusta padding

--- src/screens/RequestPermissions/index.tsx
+++ src/screens/RequestPermissions/index.tsx
@@ -32,7 +32,7 @@ export default function RequestPermissions() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 20,
+    padding: 20,
     backgroundColor: COLORS.white,
     alignItems: 'center',
     justifyContent: 'space-around',
