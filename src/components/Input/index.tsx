dd148b19e2a091fa20a5b507a5368d9c3f29a341
Vinicius Santos <viniciusantos.florencio@gmail.com>
2023-04-12 20:48:21 -0300

    increment component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,37 +1,35 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import React from 'react';
-import { TextInput, View, TextInputProps } from 'react-native';
-import styled from 'styled-components/native';
+import React, { forwardRef } from 'react';
+import { TextInput, TextInputProps } from 'react-native';
+
+import { Container, TextField, ErrorMessage } from './styles';
 
 import { COLORS } from '~/globals/style';
 
 interface Props extends TextInputProps {
   iconName: keyof typeof MaterialCommunityIcons.glyphMap;
   hasError: boolean;
+  error?: string;
+  ref?: React.Ref<TextInput>;
 }
 
-const Input = ({ iconName, hasError, ...rest }: Props) => (
-  <Container>
-    <MaterialCommunityIcons
-      name={iconName}
-      size={24}
-      color={hasError ? COLORS.error : COLORS.gray}
-    />
-    <TextField {...rest} error={hasError} />
-  </Container>
+const Input = forwardRef(
+  (
+    { iconName, error, hasError, ...rest }: Props,
+    ref: React.Ref<TextInput>
+  ) => (
+    <>
+      <Container>
+        <MaterialCommunityIcons
+          name={iconName}
+          size={24}
+          color={hasError ? COLORS.error : COLORS.gray}
+        />
+        <TextField {...rest} error={hasError} ref={ref} />
+      </Container>
+      {error && hasError ? <ErrorMessage>{error}</ErrorMessage> : null}
+    </>
+  )
 );
-
-const Container = styled(View)`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const TextField = styled(TextInput)`
-  width: 100%;
-  border-bottom-width: 1px;
-  padding: 20px;
-  border-color: ${({ error }: { error: boolean }) =>
-    error && `${COLORS.error}`};
-`;
 
 export default Input;
