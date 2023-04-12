7bf07247fb12dcfb1b54868afc7808231e84e929
Vinicius Santos <viniciusantos.florencio@gmail.com>
2023-04-12 18:12:14 -0300

    remove unneed configuration

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import devToolsEnhancer from 'remote-redux-devtools';
 
 import notificationReducer from './reducers/notification';
 
@@ -7,8 +6,6 @@ export const store = configureStore({
   reducer: {
     notificationReducer,
   },
-  devTools: true,
-  enhancers: [devToolsEnhancer({ realtime: true, port: 8000 })],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
